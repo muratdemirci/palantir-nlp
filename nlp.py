35fd59fcd7bd6f152e02772804fe762a10fa8154
Ahmet Doğukan Yıldıran <adogukanyildiran@protonmail.com>
2022-09-17 02:13:53 +0300

    NLP implementation with real data

--- nlp.py
+++ nlp.py
@@ -1,3 +1,23 @@
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+# You'll need to download this the first time it runs
+# nltk.download([
+#     "names",
+#     "stopwords",
+#     "state_union",
+#     "twitter_samples",
+#     "movie_reviews",
+#     "averaged_perceptron_tagger",
+#     "vader_lexicon",
+#     "punkt",
+# ])
+
+sia = SentimentIntensityAnalyzer()
+
+def get_sentiment(text: str)->float:
+    #"Returns a value between -1 and 1, where negative is bad and positive is good"
+    return sia.polarity_scores(text)["compound"]
 
 def score(text):
-    return text
+    return get_sentiment(text)
